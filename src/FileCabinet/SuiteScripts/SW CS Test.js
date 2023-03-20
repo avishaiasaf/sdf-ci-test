0fc5a4ce484595db2ff0b4beeb541b3eea62aadb
Avishai Asaf <avishai.asaf@similarweb.com>
2023-03-20 17:35:52 +0200

    pipeline stages

--- src/FileCabinet/SuiteScripts/SW CS Test.js
+++ src/FileCabinet/SuiteScripts/SW CS Test.js
@@ -16,7 +16,7 @@ define([],
          * @since 2015.2
          */
         EntryPoints.beforeLoad = (scriptContext) => {
-            log.debug('Test', 'chache is working')
+            log.debug('Test', 'pipeline stages succeeded')
         }
 
         /**
