a8a6081462ba8abc7aa8eb278df1c7b23e866c1e
Avishai Asaf <avishai.asaf@similarweb.com>
2023-03-21 14:14:36 +0200

    diff action

--- src/FileCabinet/SuiteScripts/SW CS Test.js
+++ src/FileCabinet/SuiteScripts/SW CS Test.js
@@ -17,7 +17,7 @@ define([],
          */
         EntryPoints.beforeLoad = (scriptContext) => {
             log.debug('Test', 'pipeline stages succeeded');
-            log.debug('Test', 'deploy file 2');
+            log.debug('Test', 'deploy file 3');
         }
 
         /**
